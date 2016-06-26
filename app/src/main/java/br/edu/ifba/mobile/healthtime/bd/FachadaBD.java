12754d728c1c2861ff7b1e5df74d2fc30776b976
deaple <yzaak.silva@gmail.com>
2016-06-25 22:11:05 -0300

    Adição da classe que manipula medicamentos, comunicação com banco SQLite e CRUD

---
+++ app/src/main/java/br/edu/ifba/mobile/healthtime/bd/FachadaBD.java
@@ -0,0 +1,121 @@
+package br.edu.ifba.mobile.healthtime.bd;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * Created by isaac on 25/06/16.
+ */
+public class FachadaBD extends SQLiteOpenHelper{
+
+    private static FachadaBD instancia = null;
+    private static String NOME_BANCO = "HorarioMedicamentos";
+    private static int VERSAO_BANCO = 1;
+    private static String COMANDO_CRIACAO_TABELA_MEDICAMENTOS =
+            "CREATE TABLE MEDICAMENTO(codigo INTEGER PRIMARY KEY AUTO INCREMENT, " +
+                    "nomeMedicamento TEXT, nomeFarmacia TEXT, telFarmacia VARCHAR(14), nroLembretesDiarios INTEGER, " +
+                    "quantidadeDiaria INTEGER, horarioInicial DATETIME, restricoes TEXT)";
+
+    public FachadaBD(Context contexto){
+        super(contexto,NOME_BANCO,null,VERSAO_BANCO);
+    }
+
+    public static FachadaBD criarInstancia(Context contexto){
+        if(instancia == null){
+            instancia = new FachadaBD(contexto);
+        }
+
+        return instancia;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(COMANDO_CRIACAO_TABELA_MEDICAMENTOS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    //CRUD
+    public long inserir(Medicamento medicamento){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues valores = new ContentValues();
+
+        valores.put("nomeMedicamento", medicamento.getNomeMedicamento());
+        valores.put("nomeFarmacia", medicamento.getNomeFarmacia());
+        valores.put("telFarmacia",medicamento.getTelFarmacia());
+        valores.put("nroLembretesDiarios", medicamento.getNroLembretesDiarios());
+        valores.put("quantidadeDiaria", medicamento.getQuantidadeDiaria());
+        valores.put("horarioInicial", medicamento.getHorarioInicial().toString());
+        valores.put("restricoes",medicamento.getRestricoes());
+
+        long codigo = db.insert("HorarioMedicamentos", null, valores);
+        return codigo;
+    }
+
+    public List<Medicamento> listarMedicamentos(){
+        List<Medicamento> medicamentos = new ArrayList<Medicamento>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selecao = "SELECT codigo, nomeMedicamento, nomeFarmacia,telFarmacia, nroLembretesDiarios," +
+                " quantidadeDiaria, horarioInicial, restricoes";
+
+        Cursor cursor = db.rawQuery(selecao,null);
+
+        if(cursor != null){
+            boolean temProximo = cursor.moveToFirst();
+
+            while (temProximo){
+                Medicamento medicamento = new Medicamento();
+                medicamento.setCodigo(cursor.getLong(cursor.getColumnIndex("codigo")));
+                medicamento.setNomeMedicamento(cursor.getString(cursor.getColumnIndex("nomeMedicamento")));
+                medicamento.setNomeFarmacia(cursor.getString(cursor.getColumnIndex("nomeFarmacia")));
+                medicamento.setTelFarmacia(cursor.getString(cursor.getColumnIndex("telFarmacia")));
+                medicamento.setNroLembretesDiarios(cursor.getInt(cursor.getColumnIndex("nroLembretesDiarios")));
+                medicamento.setQuantidadeDiaria(cursor.getInt(cursor.getColumnIndex("quantidadeDiaria")));
+                String horario = cursor.getString(cursor.getColumnIndex("horarioInicial"));
+                //converter String em tempo
+                medicamento.setHorarioInicial(Calendar.getInstance());
+
+                medicamentos.add(medicamento);
+                temProximo = cursor.moveToNext();
+            }
+        }
+
+        return medicamentos;
+    }
+
+    public long atualizar(Medicamento medicamento){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues valores = new ContentValues();
+
+        valores.put("nomeMedicamento", medicamento.getNomeMedicamento());
+        valores.put("nomeFarmacia", medicamento.getNomeFarmacia());
+        valores.put("telFarmacia",medicamento.getTelFarmacia());
+        valores.put("nroLembretesDiarios", medicamento.getNroLembretesDiarios());
+        valores.put("quantidadeDiaria", medicamento.getQuantidadeDiaria());
+        valores.put("horarioInicial", medicamento.getHorarioInicial().toString());
+        valores.put("restricoes",medicamento.getRestricoes());
+
+        long codigo = db.update("HorarioMedicamentos", valores, "codigo = "+medicamento.getCodigo(), null);
+
+        return codigo;
+    }
+
+    public int remover(Medicamento medicamento){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        int codigo = db.delete("HorarioMedicamentos", "codigo = "+medicamento.getCodigo(), null);
+        return codigo;
+    }
+}
+
---
+++ app/src/main/java/br/edu/ifba/mobile/healthtime/bd/Medicamento.java
@@ -0,0 +1,94 @@
+package br.edu.ifba.mobile.healthtime.bd;
+
+import java.util.Calendar;
+
+/**
+ * Created by isaac on 25/06/16.
+ */
+public class Medicamento {
+    private String nomeMedicamento;
+    private String nomeFarmacia;
+    private String telFarmacia;
+    private int nroLembretesDiarios;
+    private int quantidadeDiaria;
+    private Calendar horarioInicial;
+    private String restricoes;
+    private long codigo;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Calendar getHorarioInicial() {
+        return horarioInicial;
+    }
+
+    public void setHorarioInicial(Calendar horarioInicial) {
+        this.horarioInicial = horarioInicial;
+    }
+
+    public String getRestricoes() {
+        return restricoes;
+    }
+
+    public void setRestricoes(String restricoes) {
+        this.restricoes = restricoes;
+    }
+
+    public int getQuantidadeDiaria() {
+        return quantidadeDiaria;
+    }
+
+    public void setQuantidadeDiaria(int quantidadeDiaria) {
+        this.quantidadeDiaria = quantidadeDiaria;
+    }
+
+    public int getNroLembretesDiarios() {
+        return nroLembretesDiarios;
+    }
+
+    public void setNroLembretesDiarios(int nroLembretesDiarios) {
+        this.nroLembretesDiarios = nroLembretesDiarios;
+    }
+
+    public String getTelFarmacia() {
+        return telFarmacia;
+    }
+
+    public void setTelFarmacia(String telFarmacia) {
+        this.telFarmacia = telFarmacia;
+    }
+
+    public String getNomeFarmacia() {
+        return nomeFarmacia;
+    }
+
+    public void setNomeFarmacia(String nomeFarmacia) {
+        this.nomeFarmacia = nomeFarmacia;
+    }
+
+    public String getNomeMedicamento() {
+        return nomeMedicamento;
+    }
+
+    public void setNomeMedicamento(String nomeMedicamento) {
+        this.nomeMedicamento = nomeMedicamento;
+    }
+
+    public void Medicamento(){
+
+    }
+
+    public Calendar CalculaHorario(){
+        /*
+            calcula o horário de ingerir cada medicamento
+            com base na quantidade de doses diárias e horário inicial
+         */
+        return null;
+    }
+
+}
